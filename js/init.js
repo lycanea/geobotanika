d3fba9e397119251eae3a85c5b058396d8d51bac
Jakub Štenc <33066855+lycanea@users.noreply.github.com>
2019-03-17 14:57:55 +0100

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -10,7 +10,7 @@
 		reset: 'full',
 		breakpoints: {
 			global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1500, grid: { gutters: 40 } },
+			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
 			normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, lockViewport: true },
 			narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 25 } },
 			narrower:	{ range: '-840', href: 'css/style-narrower.css', grid: { collapse: true } },
@@ -29,7 +29,7 @@
 					orientation: 'vertical',
 					position: 'top-left',
 					side: 'left',
-					width: 445
+					width: 275
 				},
 				titleBar: {
 					breakpoints: 'narrower',
